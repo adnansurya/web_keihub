bcad54f960af424f613c7833254c1d77bd788cec
Adnan <muh.adnansuryaazis@gmail.com>
2018-10-16 21:06:37 +0800

    add kategori with vue

---
+++ public/js/firebase_config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAKJ1_VfSWArIlkq56rcHnCRiqKV8MYI4M",
+    authDomain: "keihubweb.firebaseapp.com",
+    databaseURL: "https://keihubweb.firebaseio.com",
+    projectId: "keihubweb",
+    storageBucket: "keihubweb.appspot.com",
+    messagingSenderId: "593724962878"
+  };
+  firebase.initializeApp(config);
